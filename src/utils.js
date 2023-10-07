7625031683d2ae8bb9021697191fddfcef8b52d8
Chaitanya7666 <chaitu.y25@gmail.com>
2023-10-07 21:48:14 +0530

    Updated: date util

--- src/utils.js
+++ src/utils.js
@@ -1,17 +1,15 @@
 export const timeStampToLocaleString = (timeStamp) => {
-    const date = new Date(timeStamp);
+  const date = new Date(timeStamp);
 
-    const istDate = date.toLocaleString("en-IN", {
-      timeZone: "Asia/Kolkata",
-      month: "short",
-      day: "numeric",
-      year: "numeric",
-      hour: "numeric",
-      minute: "numeric",
-      second: "numeric",
-      hourCycle: "h23",
-      timeZoneName: "short",
-    });
-    console.log(istDate);
-    return istDate;
-  };
+  const istDate = date.toLocaleString("en-IN", {
+    timeZone: "Asia/Kolkata",
+    month: "short",
+    day: "numeric",
+    year: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    hourCycle: "h12",
+    timeZoneName: "short",
+  });
+  return istDate;
+};
